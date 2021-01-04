4b7d65c0260ee7d0243fd0a8b7305842154beaa8
HopeNeely <hope.neely@gmail.com>
2021-01-04 06:51:44 -0500

    added print commands for final variables; no new code

--- PyBank/main.py
+++ PyBank/main.py
@@ -4,20 +4,44 @@ import csv
 # Store the file path associated with the PyBank Challenge
 file = os.path.join('Resources', 'budget_data.csv')
 
-#Open the file in "read" mode ('r') and store the contents in the variable csv
+#Open the file store the contents in the variable csv
 with open(file) as csvfile:
     #CSV reader specifies delimerter and variable that holds contents
     csvreader = csv.reader(csvfile, delimiter = ",") 
 
     #Read the header row first
     csv_header = next(csvreader)
-    print(f"CSV Header:{csv_header}")
+    #print(f"CSV Header:{csv_header}")
 
-    #Read each row of data after the header
+    #loop each row of data after the header
     for row in csvreader:
-        print(row)
+
+        
+        
+    #Define the function and have it accept the 'row' as its sole parameter
+    
+
+
+
+        #Write a function that returns the arithmetic average
+#        def average(numbers):
+#            length = len(numbers)
+#            total = 0.0
+#            for number in numbers:
+#                total += number
+#            return total / length
+
+
 
     #Look for greatest increase in profits. Then print with date and amount.
     # for row in csvreader: 
     #     if row[0] == max.SOMETHING...that is the : greatest_change 
     #           print(f'Greatest Increase in Profits: {row[0]} {row[1] )   
+
+print("Financial Analysis")
+print("-----------------------------")
+#print(f"Total Months: {total_months}")
+#print(f"Total: {total_profitloss}")
+print(f"Average Change: {average(profits)}")
+#print(f"Greatest Increase in Profits: {max_date} {max_profit}")
+#print(f"Greatest Decrease in Profits: {min_date} {min_profit}")
